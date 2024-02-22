acb4e675664516721c9053ce71e1903ecc98721b
Nobitahyper <74531405+Nobitahyper@users.noreply.github.com>
2024-02-22 14:28:49 -0500

    Time: 83 ms (46.88%), Space: 19.4 MB (83.50%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -10,22 +10,25 @@ class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         
         minHeap = []
-        # Custom comparator for the heap to compare ListNode objects
-        ListNode.__lt__ = lambda self, other: self.val < other.val
+        
+        ListNode.__lt__ = lambda self, other: self.val < other.val 
         
         for l in lists:
             if l:
                 heapq.heappush(minHeap, l)
-                
+        
         dummy = ListNode(0)
-        current = dummy
+        curr = dummy
         
         while minHeap:
             node = heapq.heappop(minHeap)
-            current.next = node
-            current = current.next
+            curr.next = node
+            curr = curr.next
             if node.next:
                 heapq.heappush(minHeap, node.next)
                 
         return dummy.next
+    
+    
+   
         
